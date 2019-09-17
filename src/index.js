24c920eb72fdbd2a9f3e13b331a77494f8818e58
rinat-90 <ribroff@gmail.com>
2019-09-17 01:30:37 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// JS
+import './js/'
+
+// SCSS
+import 'bootstrap-4-grid'
+import './assets/scss/main.scss'
+
+
+// CSS (example)
+// import './assets/css/main.css'
+
+// Vue.js
+// window.Vue = require('vue')
+//
+// // Vue components (for use in html)
+// Vue.component('example-component', require('./components/Example.vue').default)
+//
+// // Vue init
+// const app = new Vue({
+//   el: '#app'
+// })
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import { slick } from 'slick-carousel';
+
+
+$(document).ready(function () {
+  //alert('yahooooo')
+
+
+
+
+})
